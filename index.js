60007cc51ad2dec3978aa3d2afc68fd088c75f18
Jose Real <joserealdev@gmail.com>
2020-02-09 19:12:23 +0100

    Delete useless methods

--- index.js
+++ index.js
@@ -17,20 +17,6 @@ bot.onText(/\/checknow/, (msg, match) => {
     });
 });
 
-bot.onText(/^(?!\/addroute)/, (msg, match) => {});
-
-bot.onText(/\/deleteroute(.*)/, (msg, match) => {});
-
-bot.onText(/\/addroute/, (msg, match) => {});
-
-bot.onText(/\/help/, (msg, match) => {
-  sendHelp(msg.chat.id);
-});
-
-bot.onText(/\/start/, (msg, match) => {
-  sendHelp(msg.chat.id);
-});
-
 cron.schedule("0 7,11,15,18,21 * * *", () => {
   checkNow().then(res => {
     res.forEach(clientData => {
